e3a17316e2eefdeba273d352406b641a04b2d5e3
sasshuura <103129552+sasshuura@users.noreply.github.com>
2025-08-28 07:50:09 -0400

    Update eje5.cpp

--- eje5.cpp
+++ eje5.cpp
@@ -1,41 +1,52 @@
-#include <iostream>
 
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    int size = 15;
+int main() {
+    int N;
+    cout << "Ingrese un número impar N: ";
+    cin >> N;
 
- 
-    int** matrix = new int*[size];
-
-    for (int i = 0; i < size; i++) {
-        matrix[i] = new int[size];
+    if (N % 2 == 0 || N < 1) {
+        cout << "N debe ser un número impar positivo." << endl;
+        return 1;
     }
 
+  
+    int** matrix = new int*[N];
+    int* cols = new int[N]; 
 
-    int rows = size; 
-    int cols = size; 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            matrix[i][j] = 0;
-        }
+   
+    for (int i = 0; i < N; i++) {
+        int num = (i <= N/2) ? (N/2 + 1 - i) + i : (i - N/2 + 1);
+        cols[i] = N/2 + 1 - abs(N/2 - i);
+        matrix[i] = new int[cols[i]];
     }
 
    
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < cols[i]; j++) {
+            matrix[i][j] = j + 1;
+        }
+    }
+
+    
+    for (int i = 0; i < N; i++) {
+       
+        int espacios = N/2 - min(i, N-1-i);
+        for (int e = 0; e < espacios; e++) cout << "  ";
+        for (int j = 0; j < cols[i]; j++) {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
     }
 
     
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < N; i++) {
         delete[] matrix[i];
     }
     delete[] matrix;
+    delete[] cols;
 
     return 0;
-
 }
