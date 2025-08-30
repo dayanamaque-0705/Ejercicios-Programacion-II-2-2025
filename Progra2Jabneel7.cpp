e951a5facee5278c414dfdb22b4cf2369e01ec72
JabneelEspinozaOlazo <jabneel.espinoza@ucb.edu.bo>
2025-08-30 09:06:36 -0400

    Create Progra2Jabneel7.cpp

---
+++ Progra2Jabneel7.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+#include <conio.h>
+
+using namespace std;
+
+int main()
+
+{
+
+    int size = 5;
+
+
+    // Creación
+
+    int** matrix = new int* [size];
+
+   // int* array = new int[size];
+
+
+    // Asignar valores
+
+    int rows = size; // filas
+
+    int cols = size; // columnas
+
+
+
+    int N = size;     // 5
+
+    int M = size - 2;  // 3
+
+
+    // Asignar valores 1
+
+    for (int i = 0; i < rows; i++) {
+
+        if(i<N){
+
+          *(matrix + i) = new int[N];
+
+        }else{
+
+          *(matrix + i) = new int[M];
+
+        }
+
+
+        for (int j = 0; j < cols; j++) {
+
+            if ( i < M || j < M){
+
+                *(*(matrix + i) + j) = i+1;
+
+            }
+
+        }
+
+    }
+
+    // Printear valores
+
+    for (int i = 0; i < rows; i++) {
+
+        for (int j = 0; j < cols; j++) {
+
+            // cout << matrix[i][j] << " ";
+
+            cout << *(*(matrix + i) + j) << " ";
+
+        }
+
+        cout << endl;
+
+    }
+
+
+    // Liberar memoria
+
+    for (int i = 0; i < size; i++) {
+
+        //delete[] matrix[i];
+
+        delete[] *(matrix + i);
+
+    }
+
+    delete[] matrix;
+
+
+    return 0;
+
+}
